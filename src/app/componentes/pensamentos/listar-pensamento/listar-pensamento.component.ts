e4e1521ff26bdf03bda742e5ca674003c14d3a5c
LeandroAmaralC <leandrocorreia35@icloud.com>
2024-03-13 20:29:10 -0300

    projeto memoteca

---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+   listaPensamentos: Pensamento[] = [
+  //  {
+  //   id: 1,
+  //   conteudo: 'Passo informações para o componente filho',
+  //   autoria: 'componente pai',
+  //   modelo: 'modelo3'
+  // },
+  // {
+  //   id: 2,
+  //   conteudo: 'Minha propriedade é decorada com @input',
+  //   autoria: 'Componente filho',
+  //   modelo: 'modelo3'
+  // },
+  // {
+  //   id: 3,
+  //   conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+  //   autoria: 'Componente filho',
+  //   modelo: 'modelo3'
+  // }
+]
+
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
+
+}
